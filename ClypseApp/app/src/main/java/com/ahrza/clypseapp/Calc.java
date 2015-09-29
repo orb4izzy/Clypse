3fdd170ae6b1a1abae9138d63c3191f1c718ffec
Medbolf <Erlan1993@inbox.ru>
2015-09-29 18:07:45 +0600

    сделан toolbar но работает только на api20  и выше

--- ClypseApp/app/src/main/java/com/ahrza/clypseapp/Calc.java
+++
@@ -1,7 +0,0 @@
-package com.ahrza.clypseapp;
-
-/**
- * Created by Erlan on 23.09.2015.
- */
-public class Calc {
-}
--- ClypseApp/app/src/main/java/com/ahrza/clypseapp/MainActivity.java
+++ ClypseApp/app/src/main/java/com/ahrza/clypseapp/MainActivity.java
@@ -1,37 +1,36 @@
 package com.ahrza.clypseapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu_toolbar);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
